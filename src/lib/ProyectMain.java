40be015c7e2f2986b2390d02dd9636ecf7c8e426
jcvillotab <jcvillotab@unal.edu.co>
2020-11-11 07:44:39 -0500

    Revert "Merge pull request #5 from AlejoM1908/developer"
    This reverts commit f68799a5a88f985582e65f019deaa98c7ce84f2c, reversing
    changes made to 6d69117c37930c366dbe8b0cce43a9c0f956a74d.

--- src/lib/ProyectMain.java
+++ src/lib/ProyectMain.java
@@ -1,42 +1,7 @@
 package lib;
 
-import java.io.IOException;
-import java.util.Scanner;
-import java.util.StringTokenizer;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import lib.automatons.AFD;
-
 public class ProyectMain {
-    
     public static void main(String[] args){
         
-        
-        /*Scanner sc = new Scanner(System.in);
-        
-        
-        String line = sc.nextLine();
-        
-        StringTokenizer tokenizer = new StringTokenizer(line, " :>,");
-        while( tokenizer.hasMoreElements()){
-            System.out.println(tokenizer.nextToken());
-        }*/
-        
-        
-        AFD prueba = new AFD();
-        try {
-            prueba.initializeAFD("C:\\Users\\Armageddon132\\Documents\\NetBeansProjects\\TeoriaProyecto\\txtTest\\AFD.txt");
-        } catch (IOException ex) {
-            Logger.getLogger(ProyectMain.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        /*
-        prueba.showAlphabet();
-        prueba.showDelta();
-        prueba.showFinalStates();
-        prueba.showInitialState();
-        prueba.showStates();
-        */
-        prueba.processString("abab", true);
     }
-    
 }
