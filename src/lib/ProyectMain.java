b1b765ad070b1c867430ad3e4b77eaf5384e29d6
jcvillotab <jcvillotab@unal.edu.co>
2020-11-11 08:26:50 -0500

    Fixing AFD

--- src/lib/ProyectMain.java
+++ src/lib/ProyectMain.java
@@ -1,7 +1,26 @@
 package lib;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import lib.automatons.AFD;
+
 public class ProyectMain {
     public static void main(String[] args){
         
+        AFD prueba = new AFD();
+        try {
+            prueba.initializeAFD("C:\\Users\\Armageddon132\\Documents\\NetBeansProjects\\TeoriaProyecto\\txtTest\\AFD.txt");
+        } catch (IOException ex) {
+            Logger.getLogger(ProyectMain.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        /*
+        prueba.showAlphabet();
+        prueba.showDelta();
+        prueba.showFinalStates();
+        prueba.showInitialState();
+        prueba.showStates();
+        */
+        prueba.processString("abab", true);
     }
 }
