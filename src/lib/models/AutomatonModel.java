d8748981bb0d922e2c31963d416617b24af2f80c
Gustavo Mojica <55957159+ImGAMP@users.noreply.github.com>
2020-12-10 19:30:27 -0500

    Update AutomatonModel.java

--- src/lib/models/AutomatonModel.java
+++ src/lib/models/AutomatonModel.java
@@ -91,6 +91,62 @@ public class AutomatonModel {
 
         return resultString;
     }
+    
+    public String tostring(){
+        if (this._alphabet.contains('$'))
+            this._alphabet.remove((Character)'$');
+
+        if (this._firstStackAlphabet.contains('$'))
+            this._firstStackAlphabet.remove((Character)'$');
+
+        String[] alphabet = this._alphabet.toString().split("\\[|,|\\]| ");
+        String[] statesList = this._statesList.toString().split("\\[|,|\\]| ");
+        String[] acceptanceStates = this._acceptanceStates.toString().split("\\[|,|\\]| ");
+        String[] firstStackAlphabet = this._firstStackAlphabet.toString().split("\\[|,|\\]| ");
+        String resultString = "";
+
+        resultString += "#alphabet\n";
+        for (String s: alphabet){
+            if (s.compareTo("") != 0) resultString += s + "\n";
+        }
+
+        resultString += "#states\n";
+        for (String s: statesList){
+            if (s.compareTo("") != 0) resultString += s + "\n";
+        }
+
+        resultString += "#initial\n";
+        resultString += this._initialState + "\n";
+
+        resultString += "#accepting\n";
+        for (String s: acceptanceStates){
+            if (s.compareTo("") != 0) resultString += s + "\n";
+        }
+
+        if (firstStackAlphabet.length > 0){
+            resultString += "#firstStackAlphabet\n";
+            for (String s: firstStackAlphabet){
+                if (s.compareTo("") != 0) resultString += s + "\n";
+            }
+        }
+
+        if (this._transitionFunction != null){
+            resultString += "#transitions\n";
+            for (Map.Entry<String, Map<Character,ArrayList<TransitionModel>>> entry1: this._transitionFunction.entrySet()){
+                for (Map.Entry<Character,ArrayList<TransitionModel>> entry2: entry1.getValue().entrySet()){
+                    ArrayList<TransitionModel> value =  entry2.getValue();
+                    for (TransitionModel transitionModel: value){
+                        resultString += transitionModel.toString() + "\n";
+                    }
+                }
+            }
+        }
+        else
+            resultString += "#transitions\n";
+
+        return resultString;
+    }
+
 
     public List<Character> alphabet() {return _alphabet;}
     public List<String> statesList() {return _statesList;}
